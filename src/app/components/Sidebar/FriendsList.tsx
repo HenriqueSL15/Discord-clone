98cf395407e140e5137a38fe59d43fe9150e5a3a
Henrique Siqueira Lima <henrynyyls@gmail.com>
2026-01-17 12:22:56 -0300

    Highlight selected friend in FriendsList
    Adds visual feedback to indicate the currently selected friend in the FriendsList component by updating button and text styles based on the selected page.
    Refactors code to improve readability and maintainability.

--- src/app/components/Sidebar/FriendsList.tsx
+++ src/app/components/Sidebar/FriendsList.tsx
@@ -17,6 +17,7 @@ export default function FriendsList({
   }, [friendships, setFriendships]);
 
   const updatePage = useUserStore((state) => state.setPage);
+  const page = useUserStore((state) => state.page);
   const validFriendships = friendships?.filter(
     (f) => f.status != "PENDING" && f.status != "BLOCKED"
   );
@@ -26,28 +27,41 @@ export default function FriendsList({
       <h1 className="text-[#6f7c8c] font-bold text-lg">MENSAGENS DIRETAS</h1>
       <div className="w-full flex-1 flex flex-col mt-3 gap-2">
         {validFriendships?.map((friendship: FriendshipWithUsers, i: number) => {
+          const otherUserId =
+            friendship.sender.id == user?.id
+              ? friendship.receiver.id
+              : friendship.sender.id;
+
           return (
             <button
               key={i}
-              className="flex gap-3 items-center hover:bg-[#3a3d4a] w-full py-1 px-1 h-full cursor-pointer transition-all rounded-lg group"
-              onClick={() =>
-                updatePage(
-                  friendship.sender.id == user?.id
-                    ? friendship.receiver.id
-                    : friendship.sender.id
-                )
-              }
+              className={`${
+                page == otherUserId ? "bg-[#3a3d4a]" : ""
+              } flex gap-3 items-center hover:bg-[#3a3d4a] w-full py-1 px-1 h-full cursor-pointer transition-all rounded-lg group`}
+              onClick={() => updatePage(otherUserId)}
             >
               <div className="w-12 h-12 bg-black rounded-full relative">
                 <div className="absolute right-0 bottom-0 bg-white w-3 h-3 rounded-full"></div>
               </div>
               <div className="flex flex-col text-start">
-                <h1 className="text-lg text-[#6f7c8c] font-bold group-hover:text-white transition-all">
+                <h1
+                  className={`${
+                    page == otherUserId
+                      ? "text-white"
+                      : "group-hover:text-white"
+                  } text-lg text-[#6f7c8c] font-bold  transition-all`}
+                >
                   {friendship.sender.id == user?.id
                     ? friendship.receiver.username
                     : friendship.sender.username}
                 </h1>
-                <h2 className="text-sm text-[#525c69] group-hover:text-white transition-all">
+                <h2
+                  className={`${
+                    page == otherUserId
+                      ? "text-white"
+                      : "group-hover:text-white"
+                  } text-sm text-[#525c69] transition-all`}
+                >
                   Online
                 </h2>
               </div>
